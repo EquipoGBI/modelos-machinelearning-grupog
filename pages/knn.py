bfe1f343b2a480f586f6aabbbde0a06107e76efa
Carlos Tarmeno <carlos-100-800@hotmail.com>
2022-12-10 00:03:15 -0500

    KNN

---
+++ pages/knn.py
@@ -0,0 +1,67 @@
+from sklearn.metrics import accuracy_score
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.neighbors import KNeighborsClassifier
+import yfinance as yf
+plt.style.use('seaborn-darkgrid')
+import warnings
+warnings.filterwarnings("ignore")
+from sklearn.metrics import classification_report,confusion_matrix
+import streamlit as st
+
+intc = yf.Ticker('INTC')
+hist = intc.history(period="max", auto_adjust=True)
+hist.head()
+
+hist['Open-Close'] = hist.Open - hist.Close
+hist['High-Low'] = hist.High - hist.Low
+
+X = hist[['Open-Close', 'High-Low']]
+X.head()
+
+y = np.where(hist['Close'].shift(-1) > hist['Close'], 1, 0)
+y
+
+split_percentage = 0.7
+split = int(split_percentage*len(hist))
+
+X_train = X[:split]
+y_train = y[:split]
+
+X_test = X[split:]
+y_test = y[split:]
+
+knn = KNeighborsClassifier(n_neighbors=15)
+knn.fit(X_train, y_train)
+
+print("Predicciones del clasificador:")
+test_data_predicted = knn.predict(X_test)
+print(test_data_predicted)
+st.write(test_data_predicted)
+print("Resultados esperados:")
+print(y_test)
+st.write(y_test)
+
+print(accuracy_score(test_data_predicted, y_test))
+st.write(accuracy_score(test_data_predicted, y_test))
+
+tasa_error = []
+for i in range(1,40):
+  knn_g = KNeighborsClassifier(n_neighbors=i)
+  knn_g.fit(X_train,y_train)
+  pred_i = knn_g.predict(X_test)
+  tasa_error.append(np.mean(pred_i != y_test))
+
+fig = plt.figure(figsize=(10,6),dpi=250)
+plt.plot(range(1,40),tasa_error,color='blue', linestyle='dashed', marker='o',markerfacecolor='red', markersize=10)
+plt.title('Tasa de Error vs. Valor de K')
+plt.xlabel('K')
+plt.ylabel('Tasa de Error')
+st.pyplot(fig)
+
+knn = KNeighborsClassifier(n_neighbors=19)
+knn.fit(X_train,y_train)
+pred = knn.predict(X_test)
+print('CON K=19')
+print(classification_report(y_test,pred))
