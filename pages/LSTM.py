8a3ca092bbd7bcdff27470ff4023d7869db9d551
Yanfer <oscar.yanfer@unmsm.edu.pe>
2022-12-21 20:53:06 -0500

    lstm v4

--- pages/LSTM.py
+++ pages/LSTM.py
@@ -1,8 +1,8 @@
 import time
 import math
-from keras.models import Sequential
-from keras.layers.core import Dense, Dropout, Activation
-from keras.layers import LSTM
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers.core import Dense, Dropout, Activation
+from tensorflow.keras.layers import LSTM
 import numpy as np
 import pandas as pd
 import sklearn.preprocessing as prep
